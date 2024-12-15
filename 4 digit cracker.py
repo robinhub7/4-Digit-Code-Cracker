71cc0623779fcb74f2b8c751a97c502b42f138e7
robinhub7 <bankres@mail.com>
2024-12-15 08:36:14 +0000

    Add files via upload

---
+++ 4 digit cracker.py
@@ -0,0 +1,20 @@
+import keyboard
+import time
+
+#Create Passwords
+p_p = []
+
+for i in range(0, 10000):
+    if i < 10:
+        p_p.append("000{}".format(i))
+    elif i < 100:
+        p_p.append("00{}".format(i))
+    elif i < 1000:
+        p_p.append("0{}".format(i))
+    else:
+        p_p.append(str(i))
+
+#Entering the password - 17-20 minutes to fully finish
+for i in p_p:
+    for x in i:
+        keyboard.write(x)
